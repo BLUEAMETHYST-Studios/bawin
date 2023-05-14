8d165e2c9c247c53c0b38becfaf26de70d0ac541
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-05-14 22:12:03 +0200

    version and autoupdater updates
    - changed VERSION to VERSION.txt
    - updated autoupdater

--- autoupdate/updatechecker.ps1
+++ autoupdate/updatechecker.ps1
@@ -2,9 +2,7 @@ $url = "https://raw.githubusercontent.com/BLUEAMETHYST-Studios/bawin/main/VERSIO
 $output = Invoke-WebRequest -Uri $url
 $remote_integer = $output.Content.Trim()
 
-$local_integer = Get-Content -Path $Env:LOCALAPPDATA + "/bawin/VERSION"
+$local_integer = Get-Content -Path $Env:LOCALAPPDATA + "/bawin/VERSION.txt"
 if ($local_integer -lt $remote_integer) {
-    Write-Host "The remote integer is larger than the local integer."
-} else {
-    Write-Host "The local integer is larger than or equal to the remote integer."
+    . $Env:LOCALAPPDATA/bawin/autoupdate/updater.ps1 # server has a newer version
 }
---
+++ autoupdate/updater.ps1
@@ -0,0 +1,3 @@
+Set-Location $Env:LOCALAPPDATA
+Remove-Item "bawin"
+git clone https://github.com/BLUEAMETHYST-Studios/bawin
